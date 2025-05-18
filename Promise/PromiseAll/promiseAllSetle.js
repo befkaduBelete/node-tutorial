cbdd15fb835f6997786d47b6dc60790c3bd5066b
BefkaduB <befkadu.beletee@gmail.com>
2025-05-18 16:27:51 +0300

    Promise Allsettled

---
+++ Promise/PromiseAll/promiseAllSetle.js
@@ -0,0 +1,19 @@
+function function1(){
+   return new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("This is resolve function")
+    })
+   })
+}
+
+function function2(){
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+         reject("This is reject function ")
+       })
+    })
+}
+
+Promise.allSettled([function1(),function2()]).then(res=>{console.log(res)}).catch(er=>{
+    console.log(er)
+})
